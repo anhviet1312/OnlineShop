db4fa44339a3fba44f22dee4c70a35a16d7130e0
viethq <quocviet13122002@gmail.com>
2023-07-06 23:23:09 +0700

    init project

---
+++ Models/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ShopOnline.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime Dob { get; set; }
+    }
+}
