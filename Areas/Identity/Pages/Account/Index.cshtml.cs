f1c1a13c5266de5eced73017f9590c4eca406011
viethq <quocviet13122002@gmail.com>
2023-07-24 23:10:45 +0700

    add Profile Manage page

---
+++ Areas/Identity/Pages/Account/Index.cshtml.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShopOnline.Models;
+using ShopOnline.Binder;
+
+namespace ShopOnline.Areas.Identity.Pages.Account
+{
+    public partial class IndexModel : PageModel
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public IndexModel(
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [Display(Name = "Tên tài khoản")]
+        public string Username { get; set; }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Phone]
+            [Display(Name = "Phone Number")]
+            public string? PhoneNumber { get; set; }
+
+            [MaxLength(100)]
+            [Display(Name = "First Name")]
+            public string? FirstName { set; get; }
+
+            [MaxLength(100)]
+            [Display(Name = "Last Name")]
+            public string? LastName { set; get; }
+
+
+            [DataType(DataType.Date)]
+            [Display(Name = "Birth Day dd/mm/yyyy")]
+            [ModelBinder(BinderType = typeof(DayMonthYearBinder))]
+            [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+            public DateTime? Birthday { set; get; }
+
+        }
+
+        // Nạp thông tin từ User vào Model
+        private async Task LoadAsync(AppUser user)
+        {
+            var userName = await _userManager.GetUserNameAsync(user);
+            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            Username = userName;
+            Input = new InputModel
+            {
+                PhoneNumber = phoneNumber,
+                Birthday = user.Dob,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+            };
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                return NotFound($"Cannot load account ID = '{_userManager.GetUserId(User)}'.");
+            }
+
+            await LoadAsync(user);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                return NotFound($"Not exist account ID: '{_userManager.GetUserId(User)}'.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                await LoadAsync(user);
+                return Page();
+            }
+
+            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            if (Input.PhoneNumber != phoneNumber)
+            {
+                var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
+                if (!setPhoneResult.Succeeded)
+                {
+                    StatusMessage = "Fail updating phone number.";
+                    return RedirectToPage();
+                }
+            }
+
+            // Cập nhật các trường bổ sung
+            user.Dob = (DateTime)Input.Birthday;
+            user.FirstName = Input.FirstName;
+            user.LastName = Input.LastName;
+            await _userManager.UpdateAsync(user);
+
+            // Đăng nhập lại để làm mới Cookie (không nhớ thông tin cũ)
+            await _signInManager.RefreshSignInAsync(user);
+            StatusMessage = "Hồ sơ của bạn đã cập nhật";
+            return RedirectToPage();
+        }
+    }
+}
