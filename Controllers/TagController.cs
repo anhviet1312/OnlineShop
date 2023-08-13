1a7036e3e8e503e9a71215c7912f1f534d01ea2a
viethq <quocviet13122002@gmail.com>
2023-08-13 23:17:50 +0700

    add more nav bar for admin role

--- Controllers/TagController.cs
+++ Controllers/TagController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using ShopOnline.Interface;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace ShopOnline.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class TagController : Controller
     {
         private readonly ITagRepository _tagRepository;
